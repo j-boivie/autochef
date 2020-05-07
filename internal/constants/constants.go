122e4b91bef989b6908f61e17eb04b8ace273a3d
j-boivie <johannes@technocrati.se>
2020-05-07 14:08:56 +0200

    Add words as constants

---
+++ internal/constants/constants.go
@@ -0,0 +1,15 @@
+package constants
+
+var Verbs = []string{"Konsolidera", "Harmonisera", "Virtualisera",
+	"Outsourca", "Uppdatera", "Abstrahera", "Brainstorma kring",
+	"Implementera", "Validera", "Benchmarka", "Standardisera", "Centralisera",
+	"Decentralisera", "Streamlinea", "Verifiera", "Aktualisera",
+	"Dokumentera", "Mindmappa"}
+
+var Subjs = []string{"changeprocessen", "ITIL", "kaffebryggaren",
+	"SAP", "SCRUM", "agile development", "Jboss", "molnet", "SaaS", "SOA",
+	"NoSQL", "sharepoint", "lean IT", "apparna", "ASP.NET", "grön IT",
+	"tjänstestrukurerna", "MBWA", "clean desk", "tjänsteportföljen",
+	"en RFC", "grejerna i pipen", "turnkey-solutions", "backlogen",
+	"CRM", "polopoly", "disaster recovery-planen", "middleware",
+	"Web 2.0-tjänsterna", "HTML5", "CMS"}
